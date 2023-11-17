150825d29509b53e0a8bec158d5e2c70f13faf8d
tiffanywalker22 <tbcasper16@gmail.com>
2023-11-17 13:07:10 -0800

    first try of stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 expose the '/status' endpoint."""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
@@ -13,3 +14,16 @@ def get_status():
         Response: JSON response with status "OK".
     """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+    def get_stats():
+        """"""
+    classes = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+    return jsonify(classes)
