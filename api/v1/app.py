0080aa3d1b04d3b71044ffa7e47b8ae0c0e0be1d
Frankblation <frankblation@example.com>
2023-11-17 09:39:26 -0800

    More Docs

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,8 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+# app.teardown closes the application
+
 
 @app.teardown_appcontext
 def teardown_appcontext():
