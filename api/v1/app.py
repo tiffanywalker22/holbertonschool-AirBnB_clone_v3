e2a8939ed1502e472fe5edd698da1f736797011f
Frankblation <frankblation@example.com>
2023-11-17 09:46:29 -0800

    More Docs

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,6 @@ from flask import Flask, Blueprint, render_template
 from models import Storage
 from api.v1.views import app_views
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -17,6 +16,5 @@ def teardown_appcontext():
     """Close the storage when the application context ends."""
     storage.close()
 
-
 if __name__ == '__main__':
-    app.run(host=0.0.0.0, port=5000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
