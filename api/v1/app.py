3ae68b64300c16474afac3467d62a85afea412df
Frankblation <frankblation@example.com>
2023-11-17 09:20:59 -0800

    app documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Main script for running the Flask application."""
+
 from flask import Flask, Blueprint, render_template
 from models import Storage
 from api.v1.views import app_views
@@ -7,10 +9,12 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_appcontext():
     """Close the storage when the application context ends."""
     storage.close()
 
+
 if __name__ == '__main__':
-    app.run(#host=0.0.0.0, port=5000, debug=True, threaded=True)
+    app.run(host=0.0.0.0, port=5000, debug=True, threaded=True)
