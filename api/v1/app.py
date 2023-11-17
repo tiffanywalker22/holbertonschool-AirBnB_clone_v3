b7b19509f75bc37c4192ef7cfdbc4c853790ea32
Frankblation <frankblation@example.com>
2023-11-17 11:32:49 -0800

    host=host

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """Main script for running the Flask application."""
 
 from flask import Flask
-from models import storage
+from models.__init__ import storage
 from api.v1.views import app_views
 import os
 
@@ -18,5 +18,11 @@ def teardown_appcontext():
     """Close the storage when the application context ends."""
     storage.close()
 
+# Set the host and port based on environment variables or use default values
+
+
+host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+port = int(os.environ.get('HBNB_API_PORT', 5000))
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    app.run(host=host, port=port, debug=True, threaded=True)
