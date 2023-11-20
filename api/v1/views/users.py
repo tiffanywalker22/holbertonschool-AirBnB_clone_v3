e24caef57ce399d71a15f12f8310ab45edaf318b
tiffanywalker22 <tbcasper16@gmail.com>
2023-11-20 10:22:18 -0800

    fixing pep8

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -10,7 +10,7 @@ from models.user import User
 def get_users():
     """Retrieves the list of all User objects"""
     users = [user.to_dict()
-                 for user in storage.all(User).values()]
+             for user in storage.all(User).values()]
     return jsonify(users)
 
 
